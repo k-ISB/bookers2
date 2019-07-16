06438a87aa3c568ccc3b7b0ec68ef157eaf81c03
k-ISB <egoukister1004@outlook.com>
2019-07-16 13:00:26 +0900

    [Finish]user model controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one :plofile_image
   attachment :profile_image
+  validates :name, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
