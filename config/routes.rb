b676b804549f9d38b352de3f903056655c1661f4
k-ISB <egoukister1004@outlook.com>
2019-07-27 20:44:26 +0900

    [Finish]RSpec test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get '/home/about', to: 'home#about'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books
   root to: 'home#top'
-  get 'home/about' => 'home#about'
  end
